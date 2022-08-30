6aab73259583346905db0d9cfd4d900982a2aca3
GlaudistonNeto <glaudistonson@gmail.com>
2022-08-30 19:03:51 -0300

    ✨feature: working with knex

---
+++ index.js
@@ -0,0 +1,18 @@
+var database = require('./database');
+
+/* var data = [
+  {
+    name: "GTA",
+    price: 9.99
+  },
+  {
+    name: "WoW",
+    price: 23.97
+  }
+]
+
+database.insert(data).into("games").then(data => {
+  console.log(data);
+}).catch(err => {
+  console.log(err);
+}); */
