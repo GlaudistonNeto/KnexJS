49f8d11cbac47131b42369dc4a480b4a0b4480ae
GlaudistonNeto <glaudistonson@gmail.com>
2022-08-31 15:39:15 -0300

    ✨feature: registered Knex CRUD

--- index.js
+++ index.js
@@ -28,11 +28,46 @@ database.select(['id', 'price']).table('games').then(data => {
 */
 
 // NESTED QUERY
-database.insert({ name: "Mists of noyah", price: 25.00 }).into("games")
-  .then(data => {
-    database.select(['id', 'price']).table('games').then(data => {
+// database.insert({ name: "Mists of noyah", price: 25.00 }).into("games")
+//   .then(data => {
+//     database.select(['id', 'price']).table('games').then(data => {
+//   console.log(data);
+//     });
+// }).catch(err => {
+//   console.log(err);
+// });
+
+/* WHERE
+database.select(["id","price"])
+            .whereRaw( "name = 'Mists of noyah' OR price > 50" )
+            .table("games").then(data => {
+              console.log(data);
+            }).catch(err => {
+              console.log(err);
+            });
+*/
+
+/* RAW
+database.raw("SELECT * FROM games").then(data => {
   console.log(data);
-    });
 }).catch(err => {
   console.log(err);
 });
+*/
+
+/* DELETE
+database.where({ id: 3 }).delete().table("games").then(data => {
+  console.log(data);
+}).catch(err => {
+  console.log(err);
+});
+*/
+
+/* UPDATE
+database.where({ id: 1 }).update({ price: 49.90, name: 'Sea of Thieves' })
+        .table("games").then(data => {
+          console.log(data);
+        }).catch(err => {
+          console.log(err);
+        });
+*/
