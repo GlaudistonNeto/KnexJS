4826d52e35130de31ee7eaaec88ba919d68d9fca
GlaudistonNeto <glaudistonson@gmail.com>
2022-08-31 00:21:05 -0300

    ✨feature: handling inserts and selects

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var database = require('./database');
 
+// CREATE
 /* var data = [
   {
     name: "GTA",
@@ -16,3 +17,22 @@ database.insert(data).into("games").then(data => {
 }).catch(err => {
   console.log(err);
 }); */
+
+// SELECT
+/*
+database.select(['id', 'price']).table('games').then(data => {
+  console.log(data);
+}).catch(err => {
+  console.log(err);
+});
+*/
+
+// NESTED QUERY
+database.insert({ name: "Mists of noyah", price: 25.00 }).into("games")
+  .then(data => {
+    database.select(['id', 'price']).table('games').then(data => {
+  console.log(data);
+    });
+}).catch(err => {
+  console.log(err);
+});
